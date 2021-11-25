f0a5bb2e3c962d965cdeee8ea95f3d298975003c
Taylor Johnson <johnsontaylor6320@gmail.com>
2021-11-25 12:49:43 -0500

    second?

--- qt5loc2.py
+++ qt5loc2.py
@@ -1,5 +1,7 @@
 import sys 
+import re
 import os
+import subprocess
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit
 from PyQt5 import QtCore
 from PyQt5.Qt import Qt
@@ -12,6 +14,7 @@ class Example(QMainWindow):
 	def __init__(self):
 		super().__init__()
 		self.o = None
+		self.path = None
 		self.lineEntry = QLineEdit(self)
 		self.lineEntry.returnPressed.connect(self.run)
 
@@ -34,10 +37,25 @@ class Example(QMainWindow):
 		o = os.popen(cmd)
 		self.o = o.read()
 		print(self.o)
+		cmd = f"cd /usr/share/applications && ls -1 | grep {text}"
+		path = os.popen(cmd)
+		self.path = path.read()
+		print(self.path)
+		
 
 	def run(self):
-		a = f'''cd /usr/share/applications && cat $fn | grep "^Exec*" | awk '{gsub("Exec=", "");print}''''
-		
+		print(f"exec {self.o}")
+		a = f"""cd /usr/share/applications && cat {self.path} | grep "^Exec*" |""" 
+		b = """awk '{gsub("Exec=", "");print}'"""
+		a2 = (''.join(a.splitlines()))
+		b2 = (''.join(b.splitlines()))
+		cm2 = (a2 + b2)
+		print(cm2)
+		print("")
+		o2 = subprocess.check_output(cm2, shell=True)
+		print(o2)
+		o3 = o2.replace('\n', '')
+		print(o3)
 
 
 if __name__ == '__main__':
